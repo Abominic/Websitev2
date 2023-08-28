b0af970baa81ff396b9f5ac50e567ee78e1ecd1f
Dom <abominic@users.noreply.github.com>
2023-08-28 15:30:34 +0100

    Fixed merge conflicts and added a partial menu.

--- src/lib/components/squarestack/Game.svelte
+++ src/lib/components/squarestack/Game.svelte
@@ -23,7 +23,13 @@
 
   } from "./ssUtil";
 	import KeyIcon from "./KeyIcon.svelte";
+	import Overlay from "./Overlay.svelte";
 
+  enum GameState {
+    INTRO,
+    PLAYING,
+    END
+  }
 
   const boardSettings = {
     width: 10,
@@ -37,6 +43,8 @@
   let recent: RecentPieces;
   let score: number;
   let handleKeypress: (e: KeyboardEvent)=>void;
+  let state = GameState.INTRO;
+  let tickInterval: number;
 
   let nextPieceBoard: Board = {
     width: 6,
@@ -48,88 +56,88 @@
   const setup = () => {
     score = 0;
     recent = createRecentPieces();
-    if (browser) {
-      let curr = randomPieceSelect(recent);
-      let next = randomPieceSelect(curr[1]);
-      piece = newPiece(curr[0]);
-      nextPiece = next[0];
-      recent = next[1];
-    }
-
     board =  {
       ...boardSettings,
       state: "k".repeat(24*10)
     };
   };
 
-  setup();
-  if (browser) {
-
-    let tickFunction = () => {
-      return setInterval(()=>{
-        if (piece && nextPiece) { //This is to make TypeScrpt shush.
-          let collisionResult = pieceAction(board, piece, Direction.DOWN);
-          if (!collisionResult) {
-            solidify();
-          } else {
-            piece = collisionResult;
-          }
-        } else {
-          throw "an error occurred.";
-        }
-      }, 500);
-    };
-
-    let solidify = () => {
-      if (piece) {
-        board.state = bakePiece(board, piece);
-        let [newBoard, scoreDiff] = rowClear(board);
-        board = newBoard;
-        score += scoreDiff;
-        piece = newPiece(nextPiece);
-        [nextPiece, recent] = randomPieceSelect(recent);
-
-        if (checkForLoss(board)) {
-          //TODO change to an actual loss screen.
-          setup();
-        }
-      } 
-    };
-
-    let tickInterval = tickFunction();
-    
-    handleKeypress = (e) => {
-      if (e.code === "KeyK") { //Reset when K is pressed.
-        setup();
-      }
-
-      let direction = keyToDirection(e);
-      if (direction && piece) {
-        let newPiece = pieceAction(board, piece, direction);
-        if (newPiece) {
-          piece = newPiece;
-          if (direction === Direction.DOWN) { //Reset tick timer.
-            let testNext = pieceAction(board, newPiece, Direction.DOWN); //Check next.
-            if (testNext) {
-              clearInterval(tickInterval);
-              tickInterval = tickFunction();
-            } else {
-              solidify();
-            }
-          }
-        } else if (direction === Direction.DOWN) {
-          solidify();
-        }
-      }
-    };
-
-    onDestroy(()=> {
-      clearInterval(tickInterval);
-    });
-  } else {
-    handleKeypress = (e)=>{};
+  const setupPiece = () => {
+    let curr = randomPieceSelect(recent);
+    let next = randomPieceSelect(curr[1]);
+    piece = newPiece(curr[0]);
+    nextPiece = next[0];
+    recent = next[1];
   }
 
+  setup();
+
+  let solidify = () => {
+    if (piece) {
+      board.state = bakePiece(board, piece);
+      let [newBoard, scoreDiff] = rowClear(board);
+      board = newBoard;
+      score += scoreDiff;
+      piece = newPiece(nextPiece);
+      [nextPiece, recent] = randomPieceSelect(recent);
+
+      if (checkForLoss(board)) {
+        //TODO change to an actual loss screen.
+        setup();
+      }
+    } 
+  };
+  
+  handleKeypress = (e) => {
+    if (e.code === "KeyK") { //Reset when K is pressed.
+      setup();
+    }
+
+    let direction = keyToDirection(e);
+    if (direction && piece) {
+      let newPiece = pieceAction(board, piece, direction);
+      if (newPiece) {
+        piece = newPiece;
+        if (direction === Direction.DOWN) { //Reset tick timer.
+          let testNext = pieceAction(board, newPiece, Direction.DOWN); //Check next.
+          if (testNext) {
+            clearInterval(tickInterval);
+            tickInterval = tickFunction();
+          } else {
+            solidify();
+          }
+        }
+      } else if (direction === Direction.DOWN) {
+        solidify();
+      }
+    }
+  };
+
+  let tickFunction = () => {
+    return setInterval(()=>{
+      if (piece && nextPiece) { //This is to make TypeScrpt shush.
+        let collisionResult = pieceAction(board, piece, Direction.DOWN);
+        if (!collisionResult) {
+          solidify();
+        } else {
+          piece = collisionResult;
+        }
+      } else {
+        throw "an error occurred.";
+      }
+    }, 500);
+  };
+
+  let startGame = () => {
+    setup();
+    setupPiece();
+    state = GameState.PLAYING;
+    tickInterval = tickFunction();
+  }
+
+  onDestroy(()=> {
+    clearInterval(tickInterval);
+  });
 
   $: renderedBoard = piece?renderBoard(board, piece):board.state;
   $: renderedNext = nextPiece?renderBoard(nextPieceBoard, getNextPreview(nextPiece)):nextPieceBoard.state;
@@ -145,6 +153,20 @@
 </style>
 
 <div class="game">
+
+  {#if state === GameState.INTRO}
+    <Overlay>
+      <h3>SquareStack</h3>
+      <p>Fill up the rows with pieces to achieve the highest score possible.</p>
+      <button class="green" on:click={()=>{startGame()}}>Start</button>
+    </Overlay>
+  {:else if state === GameState.END}
+    <Overlay>
+      <h3>Game Over</h3>
+      <p>You scored: {score}.</p>
+      <button class="green" on:click={()=>{startGame()}}>Try Again</button>
+    </Overlay>
+  {/if}
   <div>
     <h2>Score: {score}</h2>
     <button on:click={setup}>Give Up<KeyIcon>K</KeyIcon></button>
---
+++ src/lib/components/squarestack/Overlay.svelte
@@ -0,0 +1,34 @@
+<script>
+  
+</script>
+
+<style>
+  .overlay-main {
+    background-color: #7f7f7fbf;
+    width: 100%;
+    height: 100vh;
+    z-index: 2;
+    position: fixed;
+    left: 0;
+    top: 0;
+    display: flex;
+    align-items: center;
+  }
+
+  .overlay-inner {
+    margin: auto;
+    padding: 30px;
+    background-color: #3f3f3f;
+    color: white;
+    width: 15em;
+    border-radius: 5px;
+  }
+</style>
+
+<div class="overlay-main">
+  <div></div>
+  <div class="overlay-inner">
+    <slot/>
+  </div>
+  <div></div>
+</div>
